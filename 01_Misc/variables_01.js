65e67827f408912cd762bd0be170fce78b1d7845
Christoffer <chri26eq@stud.kea.dk>
2025-01-31 10:41:37 +0100

    init commit

---
+++ 01_Misc/variables_01.js
@@ -0,0 +1,29 @@
+
+
+//var name = "Christoffer"
+
+const name =  "Christoffer"
+
+const me = {};
+
+me.name = "Christoffer"
+
+console.log(me.name)
+
+const hobbies = ["coding"]
+
+hobbies.push("drwing")
+
+
+console.log(typeof hobbies)
+console.log(hobbies)
+console.log(typeof name)
+
+// ' vs " vs `
+
+// console.log , vs +
+
+const assignmentDescription = "...and the value is...";
+const value = 4;
+
+console.log(assignmentDescription, value)
