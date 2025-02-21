4f183fd35f6401342d101575fbcedb9f91e78334
Christoffer <chri26eq@stud.kea.dk>
2025-02-21 10:45:35 +0100

    html server

---
+++ 05_HTML_Server/app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (requ, resp) => {
+
+});
+
+
+const PORT = 8080;
+app.listen(PORT, () => console.log("Server is running on port", PORT));
