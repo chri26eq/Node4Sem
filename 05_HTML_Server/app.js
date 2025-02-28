28682ea1bbd130c8db39d7fef41c7f45c866ed20
Christoffer <chri26eq@stud.kea.dk>
2025-02-28 09:24:25 +0100

    05-html server

--- 05_HTML_Server/app.js
+++ 05_HTML_Server/app.js
@@ -1,11 +1,46 @@
 const express = require("express");
 
+const { parties } = require("./util/partiesLibrary.js")
+
+
+
+// console.log(parties);
+
 const app = express();
 
-app.get("/", (requ, resp) => {
+app.use(express.static("public"));
 
+
+
+
+
+
+
+
+
+let visitorCount = 0;
+
+app.get("/", (requ, resp) => {
+    resp.sendFile(__dirname + "/public/frontpage/frontpage.html");
+
+    console.log("welcome frontpage")
 });
 
+app.get("/partypage", (requ, resp) => {
+    resp.sendFile(__dirname + "/public/partypage/partypage.html");
+
+    console.log("welcome partypage")
+});
+
+
+
+
+
+
+app.get("/visitorscounts", (requ, resp) => {
+    visitorCount++;
+    resp.send({data : visitorCount})
+});
 
 const PORT = 8080;
 app.listen(PORT, () => console.log("Server is running on port", PORT));
