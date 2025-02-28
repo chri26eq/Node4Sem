fd28b34f585a3f649c48ad83ca63d7b9825f578f
Christoffer <chri26eq@stud.kea.dk>
2025-02-28 09:24:51 +0100

    05-html server

---
+++ 05_HTML_Server/util/partiesLibrary.js
@@ -0,0 +1,14 @@
+const parties = require("./parties.json");
+
+// console.log(parties);
+
+const value = 1;
+
+const valueObject = {
+    value
+}
+
+module.exports = {
+  parties,
+  valueObject,
+};
